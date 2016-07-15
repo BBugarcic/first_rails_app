7f7e790292cd7b2b71019b196bc3001908416bd6
Branislav Bugarcic <bugarcic@gmail.com>
2016-07-15 11:25:11 +0200

    admin column in table users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   validates :body, :user, :product, presence: true
+  validates :body, length: { in: 4..100 }
   validates :rating, numericality: { only_integer: true }
   belongs_to :user
   belongs_to :product
