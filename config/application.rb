d76ac36f5599d80f8e67b6b31e9fbd9e0b5c3931
Branislav Bugarcic <bugarcic@gmail.com>
2016-08-14 21:33:27 +0200

    fixed redis configuration after rails upgrade

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module FirstApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
 end
 
